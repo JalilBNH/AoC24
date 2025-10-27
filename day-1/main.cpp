a10206eb32786637739b5e530aa003bb7bfdd530
JalilBNH <jalil.bnh13@gmail.com>
2025-10-27 22:03:26 +0100

    ADDED Cmake file for executable

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -3,63 +3,95 @@
 #include <fstream>
 #include <vector>
 #include <bits/stdc++.h>
-using namespace std;
+#include <tuple>
+#include "aoc/dataparser.h"
 
-int main()
+// std::tuple<std::vector<int>, std::vector<int>> twoColsParser(std::string inputPath)
+// {
+//     int num1, num2;
+//     std::vector<int> list1, list2;
+//     std::string inputLine;
+//     std::ifstream inputFile(inputPath);
+
+//     while (getline(inputFile, inputLine))
+//     {
+//         num1 = stoi(inputLine.substr(0, inputLine.find(" ")));
+//         num2 = stoi(inputLine.substr(inputLine.find(" ") + 3, inputLine.size() - 1));
+
+//         list1.push_back(num1);
+//         list2.push_back(num2);
+//     }
+
+//     inputFile.close();
+
+//     return {list1, list2};
+// }
+
+int computeDistance(std::vector<int> v1, std::vector<int> v2)
 {
+    int dist = 0;
 
-    vector<int> list1, list2;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        dist += abs(v1[i] - v2[i]);
+    }
 
-    int num1, num2, dist, simScore;
-    int j = 0;
+    return dist;
+}
+
+int computeSimilarity(std::vector <int> v1, std::vector<int> v2)
+{
+    int simScore = 0;
     int counter = 0;
+    int j = 0;
 
-    string inputLine;
-
-    ifstream inputFile("./input.txt");
-
-    while (getline(inputFile, inputLine))
+    for (int i = 0; i < v1.size(); i++)
     {
-        num1 = stoi(inputLine.substr(0, inputLine.find(" ")));
-        num2 = stoi(inputLine.substr(inputLine.find(" ") + 3, inputLine.size() - 1));
-
-        list1.push_back(num1);
-        list2.push_back(num2);
-    }
-
-    inputFile.close();
-
-    sort(list1.begin(), list1.end());
-    sort(list2.begin(), list2.end());
-
-    // first part
-    dist = 0;
-    for (int i = 0; i < list1.size(); i++)
-    {
-        dist += abs(list1[i] - list2[i]);
-    }
-    cout << "part 1 : " << dist << "\n";
-
-    // second part
-    simScore = 0;
-    for (int i = 0; i < list1.size(); i++)
-    {
-        if (i > 0 && !(list1[i] == list1[i - 1]))
+        if (i > 0 && !(v1[i] == v1[i - 1]))
         {
             counter = 0;
         }
 
-        while (list2[j] <= list1[i] && j < list2.size())
+        while (v2[j] <= v1[i] && j < v2.size())
         {
-            if (list1[i] == list2[j])
+            if (v1[i] == v2[j])
             {
                 counter++;
             }
             j++;
         }
-        simScore += counter * list1[i];
+        simScore += counter * v1[i];
     }
-    cout << "part 2 : " << simScore << "\n";
+
+    return simScore;
+}
+
+int main()
+{
+    std::tuple<std::vector<int>, std::vector<int>> listInput;
+    std::vector<int> list1, list2;
+
+    int dist, simScore;
+    int j = 0;
+    int counter = 0;
+
+    
+    listInput = aoc::twoColsParser("./day-1/input.txt");
+
+    list1 = std::get<0>(listInput);
+    list2 = std::get<1>(listInput);
+
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+
+    // first part
+    dist = computeDistance(list1, list2); 
+    std::cout << "part 1 : " << dist << "\n";
+
+    // second part
+    
+    simScore = computeSimilarity(list1, list2);
+    std::cout << "part 2 : " << simScore << "\n";
 
     return 0;
 }
