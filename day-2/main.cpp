2124a3664fd278d653f6092bac837a6cbe68d902
JalilBNH <jalil.bnh13@gmail.com>
2025-11-04 16:13:24 +0100

    🎨

--- day-2/main.cpp
+++ day-2/main.cpp
@@ -4,6 +4,43 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <bits/stdc++.h>
+
+std::vector<std::vector<int>> stringToIntTab(std::string inputPath);
+bool comp(int a, int b);
+bool checkStepCondition(int a, int b);
+bool isSorted(std::vector<int> line);
+bool checkLineStep(std::vector<int> line);
+bool isLineSafe(std::vector<int> line);
+
+int main()
+{
+    int least = 1;
+    int most = 3;
+    bool safeState;
+    int increaseState;
+    int safeCount = 0;
+
+    int diff;
+    int pDiff;
+
+    std::vector<std::vector<int>> input;
+
+    input = stringToIntTab("./input.txt");
+
+    std::cout << "Input size : " << input.size() << std::endl;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (isLineSafe(input[i]))
+        {
+            safeCount++;
+        }
+    }
+
+    std::cout << "Safe count : " << safeCount << std::endl;
+
+    return 0;
+}
 
 std::vector<std::vector<int>> stringToIntTab(std::string inputPath)
 {
@@ -29,55 +66,55 @@ std::vector<std::vector<int>> stringToIntTab(std::string inputPath)
     return input;
 }
 
-int main()
+bool comp(int a, int b)
 {
-    int least = 1;
-    int most = 3;
-    bool safeState;
-    int increaseState;
-    int safeCount = 0;
-    
+    return a > b;
+}
 
-    int diff;
-    int pDiff;
-
-    std::vector<std::vector<int>> input;
-
-    input = stringToIntTab("./test_input.txt");
-
-    std::cout << "Input size : " << input.size() << std::endl;
-    for (int i = 0; i < input.size(); i++)
+bool isSorted(std::vector<int> line)
+{
+    if (std::is_sorted(line.begin(), line.end()) || std::is_sorted(line.begin(), line.end(), comp))
     {
-        safeState = true;
-        int j = 1;
-        while (j < input[i].size() && safeState)
+        return true;
+    }
+    return false;
+}
+
+bool checkStepCondition(int a, int b)
+{
+    int diff;
+
+    diff = b - a;
+    if (abs(diff) < 1 || abs(diff) > 3)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool checkLineStep(std::vector<int> line)
+{
+
+    for (int i = 0; i < line.size() - 1; i++)
+    {
+        if (!(checkStepCondition(line[i], line[i + 1])))
         {
-            diff = input[i][j] - input[i][j - 1];
-
-            if (abs(diff) < least || abs(diff) > most)
-            {
-                safeState = false;
-            }
-
-            if (j >= 2)
-            {
-                if (std::signbit(diff) != std::signbit(pDiff))
-                {
-                    safeState = false;
-                }
-            }
-
-            pDiff = diff;
-            j++;
-        }
-
-        if (safeState)
-        {
-            safeCount++;
+            return false;
         }
     }
+    return true;
+}
 
-    std::cout << "Safe count : " << safeCount << std::endl;
+bool isLineSafe(std::vector<int> line)
+{
+    bool sorted, stepCondition;
 
-    return 0;
+    sorted = isSorted(line);
+    stepCondition = checkLineStep(line);
+
+    if (sorted && stepCondition)
+    {
+        return true;
+    }
+    return false;
 }
